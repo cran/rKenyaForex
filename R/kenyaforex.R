130ec5439a87996e792fba40b3d7def9790cbe8c
Mutua Kilai <kilaimutua@gmail.com>
2022-07-08 13:10:05 +0000

    version 0.1.0

---
+++ R/kenyaforex.R
@@ -0,0 +1,15 @@
+#' Kenya Shillings Exchange Rates.
+#'
+#' Historical Data for KSH Exchange Rates vs Major Currencies.
+#'
+#' @format A data frame with 9746 observations of 5 variables.
+#' \describe{
+#'   \item{Date}{The date of year}
+#'   \item{Currency}{The major currency: US DOLLAR, EURO, Sterling Pound, TSHS, USHS}
+#'   \item{Mean}{The average of buy and sell columns}
+#'   \item{Buy}{Buying Price for the currency in Kenya Shillings}
+#'   \item{Sell}{Selling Price for the currency in Kenya Shillings}
+#' }
+#'
+#' @source https://www.centralbank.go.ke/rates/forex-exchange-rates/
+"kenyaforex"
